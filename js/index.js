ff113c6ef227feaada94319fbe6c68fabd2f4a6d
primerlee <jxlizhibo@gmail.com>
2018-04-03 19:19:31 +0800

    修复了滑动后不自动轮播的bug

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // 页面加载完成开始执行js代码
-window.onload=function () {
+window.onload = function () {
     //顶部搜索栏
     search();
     // banner图自动轮播和滑动效果
@@ -9,20 +9,20 @@ window.onload=function () {
 };
 
 var search = function () {
-     var oSearchBox = document.querySelector(".search_box");
-     var oBanner = document.querySelector(".jd_banner");
-     var bannerHeight = oBanner.offsetHeight;
+    var oSearchBox = document.querySelector(".search_box");
+    var oBanner = document.querySelector(".jd_banner");
+    var bannerHeight = oBanner.offsetHeight;
 
-     window.onscroll=function () {
-         var oScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-         var opacity = 0;
-         if (oScrollTop <= bannerHeight){
-             opacity = 0.85*(oScrollTop/bannerHeight);
-         }else {
-             opacity = 0.85;
-         }
-         oSearchBox.style.background="rgba(216,80,92,"+opacity+")";
-     }
+    window.onscroll = function () {
+        var oScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        var opacity = 0;
+        if (oScrollTop <= bannerHeight) {
+            opacity = 0.85 * (oScrollTop / bannerHeight);
+        } else {
+            opacity = 0.85;
+        }
+        oSearchBox.style.background = "rgba(216,80,92," + opacity + ")";
+    }
 };
 //banner
 var banner = function () {
@@ -35,44 +35,45 @@ var banner = function () {
     var aLi = oIndicate.querySelectorAll("li");
 
     function addTransition() {
-        oImageBox.style.transition="all, 0.2s";
-        oImageBox.style.webkitTransition="all, 0.2s";
+        oImageBox.style.transition = "all, 0.2s";
+        oImageBox.style.webkitTransition = "all, 0.2s";
     }
 
     function removeTransition() {
-        oImageBox.style.transition="none";
-        oImageBox.style.webkitTransition="none";
+        oImageBox.style.transition = "none";
+        oImageBox.style.webkitTransition = "none";
     }
-    function setTranslate (translateX){
-        oImageBox.style.transform="translateX("+translateX+"px)";
-        oImageBox.style.webkitTransform="translateX("+translateX+"px)";
+
+    function setTranslate(translateX) {
+        oImageBox.style.transform = "translateX(" + translateX + "px)";
+        oImageBox.style.webkitTransform = "translateX(" + translateX + "px)";
     }
 
     var index = 1;
     var timer = setInterval(function () {
         index++;
         addTransition();
-        setTranslate(-index*oWidth);
+        setTranslate(-index * oWidth);
     }, 1000);
 
     oImageBox.addEventListener("transitionend", function () {
-        if (index >= 9){
+        if (index >= 9) {
             index = 1;
             removeTransition();
-            setTranslate(-index*oWidth);
-        } else if(index <=0) {
+            setTranslate(-index * oWidth);
+        } else if (index <= 0) {
             index = 8;
             removeTransition();
-            setTranslate(-index*oWidth);
+            setTranslate(-index * oWidth);
         }
 
         setPoint();
     }, false);
 
     function setPoint() {
-        for(var i = 0; i < aLi.length; i++){
+        for (var i = 0; i < aLi.length; i++) {
             aLi[i].classList.remove("active");
-            aLi[index-1].classList.add("active");
+            aLi[index - 1].classList.add("active");
         }
     }
 
@@ -88,40 +89,45 @@ var banner = function () {
     oImageBox.addEventListener('touchmove', function (e) {
         var moveX = e.touches[0].clientX;
         disX = moveX - startX;
-        var translateX = -index*oWidth + disX;
+        var translateX = -index * oWidth + disX;
         addTransition();
         setTranslate(translateX);
 
-        isMoved=true;
+        isMoved = true;
     }, false);
 
     oImageBox.addEventListener("touchend", function () {
-        if (isMoved){
-            if(Math.abs(disX) < oWidth/3){
+        if (isMoved) {
+            if (Math.abs(disX) < oWidth / 3) {
                 addTransition();
-                setTranslate(-index*oWidth);
-            }else {
-                if (disX > 0){
-                    index --;
-                } else{
-                    index ++
+                setTranslate(-index * oWidth);
+            } else {
+                if (disX > 0) {
+                    index--;
+                } else {
+                    index++
                 }
                 addTransition();
-                setTranslate(-index*oWidth);
+                setTranslate(-index * oWidth);
             }
         }
+        timer = setInterval(function () {
+            index++;
+            addTransition();
+            setTranslate(-index * oWidth);
+        }, 1000);
     }, false);
 
     clearInterval(timer);
     timer = setInterval(function () {
         index++;
         addTransition();
-        setTranslate(-index*oWidth);
+        setTranslate(-index * oWidth);
     }, 1000);
 
-    startX=0;
-    disX=0;
-    isMoved=false;
+    startX = 0;
+    disX = 0;
+    isMoved = false;
 
 };
 
@@ -130,23 +136,23 @@ var downTime = function () {
     var aSpan = oTime.querySelectorAll("span");
     var time = 10 * 60 * 60;
     timerCount();
-    var timer = setInterval(timerCount,1000);
+    var timer = setInterval(timerCount, 1000);
 
-   function timerCount () {
+    function timerCount() {
         time--;
-        var h = Math.floor(time/3600);
-        var m = Math.floor(time%3600/60);
-        var s = time%60;
+        var h = Math.floor(time / 3600);
+        var m = Math.floor(time % 3600 / 60);
+        var s = time % 60;
 
-        aSpan[0].innerText=Math.floor(h/10);
-        aSpan[1].innerText=h%10;
-        aSpan[3].innerText=Math.floor(m/10);
-        aSpan[4].innerText=m%10;
-        aSpan[6].innerText=Math.floor(s/10);
-        aSpan[7].innerText=s%10;
+        aSpan[0].innerText = Math.floor(h / 10);
+        aSpan[1].innerText = h % 10;
+        aSpan[3].innerText = Math.floor(m / 10);
+        aSpan[4].innerText = m % 10;
+        aSpan[6].innerText = Math.floor(s / 10);
+        aSpan[7].innerText = s % 10;
 
         // console.log(time);
-        if (time<=0){
+        if (time <= 0) {
             clearInterval(timer);
         }
     }
