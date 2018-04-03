d7ca7ae822b2c66108375245a2814423ec17d9e0
primerlee <jxlizhibo@gmail.com>
2018-04-03 18:27:51 +0800

    添加了banner的自动滚动和滑动操作方法

--- js/index.js
+++ js/index.js
@@ -25,7 +25,108 @@ var search = function () {
      }
 };
 var banner = function () {
-    
+    var oBanner = document.querySelector(".jd_banner");
+    var bannerWidth = oBanner.offsetWidth;
+
+    var oImageBox = oBanner.querySelector("ul:first-child");
+
+    var oIndicate = oBanner.querySelector("ul:last-child");
+    var aPoint = oIndicate.querySelectorAll("li");
+
+    //添加过渡
+    function addTransition() {
+        oImageBox.style.transition="all, 0.2s";
+        oImageBox.style.webkitTransition="all, 0,2s"
+    }
+
+    //清除过渡
+    function removeTransition() {
+        oImageBox.style.transition="none";
+        oImageBox.style.webkitTransition="none"
+    }
+
+    //设置位移
+    function setTranslate(translateX) {
+        oImageBox.style.transform="translateX("+translateX+"px)";
+        oImageBox.style.webkitTransform="translateX("+translateX+"px)";
+    }
+
+    var index = 1;
+    var timer = setInterval(function () {
+        index++;
+        addTransition();
+        setTranslate(-index*bannerWidth);
+    }, 1000);
+
+    oImageBox.addEventListener("transitionend", function () {
+        if(index>=9){
+            index = 1;
+            removeTransition();
+            setTranslate(-index*bannerWidth);
+
+        }else if(index <= 0){
+            index = 8;
+            removeTransition();
+            setTranslate(-index*bannerWidth);
+        }
+
+        setPoint();
+    }, false);
+
+    function setPoint() {
+        for (var i =0; i < aPoint.length; i++){
+            aPoint[i].classList.remove("active");
+            aPoint[index-1].classList.add("active");
+        }
+    }
+
+    var startX = 0;
+    var distanceX = 0;
+    var isMoved = false;
+
+    oImageBox.addEventListener("touchstart", function (ev) {
+        clearInterval(timer);
+        startX = ev.touches[0].clientX;
+    }, false);
+
+    oImageBox.addEventListener("touchmove", function (ev) {
+        var moveX = ev.touches[0].clientX;
+        distanceX = moveX - startX;
+
+        var translateX = (-index * bannerWidth) + distanceX;
+        removeTransition();
+        setTranslate(translateX);
+        isMoved = true;
+    }, false);
+
+    oImageBox.addEventListener("touchend", function () {
+        if(isMoved){
+            if(Math.abs(distanceX) < bannerWidth/3){
+                addTransition();
+                setTranslate(-index*bannerWidth);
+            }else {
+                if (distanceX > 0){
+                    index --;
+                }else {
+                    index++
+                }
+                addTransition();
+                setTranslate(-index*bannerWidth);
+            }
+        }
+    }, false);
+
+    clearInterval(timer);
+    timer = setInterval(function () {
+        index++;
+        addTransition();
+        setTranslate(-index*bannerWidth);
+    }, 1000);
+
+    startX = 0;
+    distanceX = 0;
+    isMoved = false;
+
 };
 var downTime = function () {
     var oTime = document.querySelector(".clock_count");
@@ -47,7 +148,7 @@ var downTime = function () {
         aSpan[6].innerText=Math.floor(s/10);
         aSpan[7].innerText=s%10;
 
-        console.log(time);
+        // console.log(time);
         if (time<=0){
             clearInterval(timer);
         }
