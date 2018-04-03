209fe09feaebf35defe1456840c213d1704d2703
primerlee <jxlizhibo@gmail.com>
2018-04-03 16:58:11 +0800

    添加了活动倒计时功能

--- js/index.js
+++ js/index.js
@@ -24,5 +24,31 @@ function banner() {
 
 }
 function downTime() {
+    var oClock = document.querySelector(".clock_count");
+    var aSpan = oClock.querySelectorAll("span");
+
+    var time = 11 * 60 *60;
+    var timer = setInterval(function () {
+        time--;
+        var h = Math.floor(time/3600);
+        var m = Math.floor(time%3600/60);
+        var s = time%60;
+
+        aSpan[0].innerText=Math.floor(h/10);
+        aSpan[1].innerText=h%10;
+
+        aSpan[3].innerText=Math.floor(m/10);
+        aSpan[4].innerText=m%10;
+
+        aSpan[6].innerText=Math.floor(s/10);
+        aSpan[7].innerText=s%10;
+
+        if (time <=0){
+            clearInterval(timer);
+        }
+    }, 1000);
+
+
+
 
 }
